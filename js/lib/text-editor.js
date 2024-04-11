f59a39f8c81e46dadc622183510293595abd9ac7
Andrej-Nil <and.kucheroff@yandex.ru>
2024-04-11 09:32:45 +0500

    Начало работы

---
+++ js/lib/text-editor.js
@@ -0,0 +1,14 @@
+class TextRedactor {
+  constructor(elementId) {
+
+    this.$container = document.querySelector(elementId);
+
+    this.init();
+  }
+
+  init = () => {
+    if (!this.$container) return;
+
+
+  }
+}
---
+++ js/main.js
@@ -0,0 +1,1 @@
+const textRedactor = new TextRedactor('#redactor');
